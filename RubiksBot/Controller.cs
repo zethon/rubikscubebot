0540622d2494ed043b687d9f0e3c25ff4d397a08
aclaure@gmail.com <aclaure@gmail.com@c3faa705-0d73-1af2-2b60-d2096b2281d4>
2010-08-28 04:14:30 +0000

    updated assembly versions added test module

---
+++ RubiksBot/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RubiksBot
+{
+    class Controller
+    {
+        public bool AppQuit = false;
+
+        public bool Init()
+        {
+            return true;
+        }
+
+        public void MainLoop()
+        {
+            string strInput = string.Empty;
+
+            do
+            {
+                strInput = Console.ReadLine();
+
+                if (strInput.Length == 0)
+                {
+                    continue;
+                }
+
+                if (strInput.ToLower() == @"quit")
+                {
+                    AppQuit = true;
+                }
+            } while (!AppQuit);
+        }
+    }
+}
